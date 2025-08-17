d0e6135d498fb974b31bfb70c63d68d5c21e005a
flexxiontop <flexxiofficial@outlook.com>
2025-08-17 14:17:55 +0600

    Update likeCommands.py

--- cogs/likeCommands.py
+++ cogs/likeCommands.py
@@ -144,8 +144,8 @@ class LikeCommands(commands.Cog):
                     else:
                         embed.description = "\n┌MAX LIKES\n└─This UID has already received the maximum likes today.\n"
 
-                    embed.set_footer(text="DEVELOPED BY THUG")
-                    embed.description += "\n🔗 JOIN : https://discord.gg/awPm5B3QFg"
+                    embed.set_footer(text="DEVELOPED BY FLEXXI")
+                    embed.description += "\n🔗 JOIN : https://discord.gg/qpsgZaTw"
                     await ctx.send(embed=embed, mention_author=True, ephemeral=is_slash)
 
         except asyncio.TimeoutError:
